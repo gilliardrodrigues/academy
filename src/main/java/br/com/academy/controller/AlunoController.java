daec35134325e45afb195d121a4fa0fc719ef1c8
Gilliard Gabriel Rodrigues <gilliard2019@ufmg.br>
2022-01-09 19:56:28 -0300

    Implementação dos filtros melhorada

--- src/main/java/br/com/academy/controller/AlunoController.java
+++ src/main/java/br/com/academy/controller/AlunoController.java
@@ -8,8 +8,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.academy.enums.Status;
 import br.com.academy.model.dao.IDaoAluno;
 import br.com.academy.model.entity.Aluno;
 
@@ -53,53 +55,17 @@ public class AlunoController
 	}
 	
 	@GetMapping("/rmAluno/{id}")
-	public String removerAluno(@PathVariable("id") Integer id)
+	public String retornaViewAlunoRemovido(@PathVariable("id") Integer id)
 	{
 		repositorio.deleteById(id);
 		return "redirect:/pesqAluno";
 	}
 	
-	@GetMapping("/filtroAlunos")
+	@GetMapping("/filtrosAluno")
 	public ModelAndView retornaViewFiltroAlunos()
 	{
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("aluno/filtroAlunos");
-		return mv;
-	}
-	
-	@GetMapping("/alunosAtivos")
-	public ModelAndView retornaViewAlunosAtivos()
-	{
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("aluno/alunosAtivos");
-		mv.addObject("alunosAtivos", repositorio.buscaAlunosAtivos());
-		return mv;
-	}
-	
-	@GetMapping("/alunosInativos")
-	public ModelAndView retornaViewAlunosInativos()
-	{
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("aluno/alunosInativos");
-		mv.addObject("alunosInativos", repositorio.buscaAlunosInativos());
-		return mv;
-	}
-	
-	@GetMapping("/alunosCancelados")
-	public ModelAndView retornaViewAlunosCancelados()
-	{
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("aluno/alunosCancelados");
-		mv.addObject("alunosCancelados", repositorio.buscaAlunosCancelados());
-		return mv;
-	}
-	
-	@GetMapping("/alunosTrancados")
-	public ModelAndView retornaViewAlunosTrancados()
-	{
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("aluno/alunosTrancados");
-		mv.addObject("alunosTrancados", repositorio.buscaAlunosTrancados());
+		mv.setViewName("aluno/filtrosAluno");
 		return mv;
 	}
 	
@@ -119,7 +85,16 @@ public class AlunoController
 		}
 		return mv;
 	}
-
+	
+	@PostMapping("filtrarAlunos")
+	public ModelAndView filtrarAlunos(@RequestParam(value = "status", required = false) Status status)
+	{
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("aluno/pesqAluno");
+		mv.addObject("alunos", repositorio.buscaAlunosFiltradoPeloStatus(status));
+		return mv;
+	}
+	
 	public boolean isTelaEdicao()
 	{
 		return telaEdicao;
--- src/main/java/br/com/academy/model/dao/IDaoAluno.java
+++ src/main/java/br/com/academy/model/dao/IDaoAluno.java
@@ -5,11 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import br.com.academy.enums.Status;
 import br.com.academy.model.entity.Aluno;
 
 public interface IDaoAluno extends JpaRepository<Aluno, Integer>
 {
-	@Query("SELECT a FROM Aluno a WHERE a.status = 'ATIVO'")
+	/*@Query("SELECT a FROM Aluno a WHERE a.status = 'ATIVO'")
 	public List<Aluno> buscaAlunosAtivos();
 	
 	@Query("SELECT a FROM Aluno a WHERE a.status = 'INATIVO'")
@@ -19,5 +20,8 @@ public interface IDaoAluno extends JpaRepository<Aluno, Integer>
 	public List<Aluno> buscaAlunosCancelados();
 	
 	@Query("SELECT a FROM Aluno a WHERE a.status = 'TRANCADO'")
-	public List<Aluno> buscaAlunosTrancados();
+	public List<Aluno> buscaAlunosTrancados();*/
+	
+	@Query("SELECT a FROM Aluno a WHERE a.status = :status")
+	public List<Aluno> buscaAlunosFiltradoPeloStatus(Status status);
 }
