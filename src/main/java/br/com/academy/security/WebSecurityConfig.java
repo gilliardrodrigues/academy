efd162facb938ae2ffef9b2b48a52426c76ecd0c
Gilliard Gabriel Rodrigues <gilliard2019@ufmg.br>
2022-02-09 09:52:29 -0300

    Correção de bug no redirecionado para o index

--- src/main/java/br/com/academy/security/WebSecurityConfig.java
+++ src/main/java/br/com/academy/security/WebSecurityConfig.java
@@ -30,7 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
                 .and()
             .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/index")
+                .defaultSuccessUrl("/index", true)
                 .permitAll()
                 .and()
             .logout()
